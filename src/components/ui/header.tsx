8cbcc475ce4547f6e88911df461f1c4671caa8eb
leye195 <leye195@naver.com>
2025-04-27 00:10:33 +0900

    feat: set home, abount, product menu on header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -14,7 +14,15 @@ export default function Header() {
           <span className="h-[30px] w-[138px] bg-black">Logo</span>
         </Link>
       </div>
-      <div className={"flex items-center"} />
+      <nav className="flex items-center">
+        <ul className="flex h-full items-center">
+          <Link href="/">
+            <li className="px-[12px]">Home</li>
+          </Link>
+          <li className="px-[12px]">About</li>
+          <li className="px-[12px]">Product</li>
+        </ul>
+      </nav>
     </header>
   );
 }
