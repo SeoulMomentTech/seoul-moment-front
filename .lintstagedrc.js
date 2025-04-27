bdc3eb3166648a863c49a6e3454105b6246c634c
leye195 <leye195@naver.com>
2025-04-27 18:08:52 +0900

    feat: update lint-stage setting

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-const prettier = (staged) => `prettier --write ${staged.join(" ")}`;
-
-const eslint = (staged) => `eslint --cache ${staged.join(" ")}`;
-
-module.exports = {
-  "*.{js,jsx,ts,tsx}": [prettier, eslint],
-  "*.{ts,tsx}": [],
-};
