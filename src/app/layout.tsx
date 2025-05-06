55aa8ba0bfa2367f6b081b085c0adeb28d2a529a
leye195 <leye195@naver.com>
2025-05-06 13:12:01 +0900

    chore: update settings.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "@/components/ui/footer";
 import Header from "@/components/ui/header";
 
-import "./globals.css";
+import "@/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
