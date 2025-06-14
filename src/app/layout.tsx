cb7c83253f857cebfaff55d3f4394ab80abce9dc
leye195 <leye195@naver.com>
2025-06-14 12:37:41 +0900

    feat:  apply Pretendard font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "@/components/ui/footer";
 import Header from "@/components/ui/header";
 
 import "@/styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Seoul Moment",
@@ -27,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Header />
         <main className="relative min-h-[calc(100vh-70px)]">{children}</main>
         <Footer />
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       className={cn(
         "z-header sticky top-0 left-0 w-full bg-white",
         "flex h-[72px] justify-between px-8",
-        "max-md:h-[48px] max-md:px-4",
+        "hidden max-md:h-[48px] max-md:px-4",
       )}
     >
       <div className={"flex items-center"}>
@@ -15,15 +15,6 @@ export default function Header() {
           <span className="h-[30px] w-[138px] bg-black">Logo</span>
         </Link>
       </div>
-      <nav className="flex items-center">
-        <ul className="flex h-full items-center">
-          <Link href="/">
-            <li className="px-[12px]">Home</li>
-          </Link>
-          <li className="px-[12px]">About</li>
-          <li className="px-[12px]">Product</li>
-        </ul>
-      </nav>
     </header>
   );
 }
